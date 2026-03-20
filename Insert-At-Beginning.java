9bb53925da34bcb016f00736ca79316a5709f565
micheal-cloud-dev <michealedison677@gmail.com>
2026-03-20 13:46:21 +0530

    code created

--- Insert-At-Beginning.java
+++ Insert-At-Beginning.java
@@ -1,1 +1,37 @@
-write code
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+public static void main (String[] args){
+
+Node head = new Node(10);
+head.next = new Node(20);
+head.next.next = new Node(30);
+
+Node current =head;
+System.out.println("Before Insert the value");
+while(current != null){
+    System.out.println(current.data);
+    current = current.next;
+}
+
+Node newHead = new Node(5);
+
+newHead.next = head;
+head = newHead;
+
+Node temp = head;
+System.out.println("After Insert the value");
+while(temp != null){
+    System.out.println(temp.data);
+    temp = temp.next;
+}
+
+}
+}
+
