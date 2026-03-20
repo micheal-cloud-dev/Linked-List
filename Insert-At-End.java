734f61c558390ac454fe5b978c706a51f639cd68
micheal-cloud-dev <michealedison677@gmail.com>
2026-03-20 22:16:38 +0530

    code updated

--- Insert-At-End.java
+++ Insert-At-End.java
@@ -1,1 +1,44 @@
-write the code
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+    public class InsertAtEndCode{
+        public static Node insertAtEnd(Node head,int data){
+           Node newNode = new Node(data);
+
+           if(head == null){
+              return newNode;
+           }
+           Node temp = head;
+           while(temp.next != null){
+            temp = temp.next;
+           }
+
+           temp.next = newNode;
+           return head;
+        }
+    }
+    public static void display(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+     }
+    public static void main(String[] args){
+
+        Node head = null;
+
+        head = insertAtEnd(head,10);
+        head = insertAtEnd(head,20);
+        head = insertAtEnd(head,30);
+        head = insertAtEnd(head,40);
+
+        display(head);
+    }
+}
